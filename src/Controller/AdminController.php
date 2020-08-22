7ec1ccc21820ebdd5c42a27ec50204b09b0c19e7
Guillaume <poucetguillaume@gmail.com>
2020-08-22 22:32:49 +0200

    add user form admin ok

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\AddUserType;
 use App\Repository\UserRepository;
 use App\Repository\CourseRepository;
 use App\Repository\SessionRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(UserRepository $userRepo, SessionRepository $sessionRepo, CourseRepository $courseRepo)
+    public function index(UserRepository $userRepo, SessionRepository $sessionRepo, CourseRepository $courseRepo, Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
+        // retrieve users info
         $users = $userRepo->findAll();
         foreach ($users as $user) {
             $userId = $user->getId();
@@ -24,13 +29,39 @@ class AdminController extends AbstractController
                 $course = $courseRepo->find($course)->getCourseName();
             }
         }
+        // dump($users);
 
-        dump($users);
+        // creating form to add users
+        $user = new User;
+
+        $form = $this->createForm(AddUserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $user = $form->getData();
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // return $this->redirectToRoute('task_success');
+        }
 
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
             'users' => $users,
-
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/AddUserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AddUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            // ->add('roles')
+            ->add('password', PasswordType::class)
+            ->add('last_name')
+            ->add('first_name')
+            // ->add('birth_date')
+            // ->add('isVerified')
+            // ->add('sessions')
+            ->add('Submit', SubmitType::class)
+ 
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
