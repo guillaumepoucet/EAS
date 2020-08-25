3f064457dd241db8cf3e4c987a0dc27f70a36120
Guillaume <poucetguillaume@gmail.com>
2020-08-25 17:25:28 +0200

    edit session ok

--- src/Form/AddSessionType.php
+++ src/Form/AddSessionType.php
@@ -2,13 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use App\Entity\Course;
 use App\Entity\Session;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AddSessionType extends AbstractType
 {
@@ -17,15 +17,16 @@ class AddSessionType extends AbstractType
         $builder
             ->add('start_date')
             ->add('end_date')
-            // ->add('user')
+            // ->add('user', EntityType::class, [
+            //     'class' => User::class
+            //     ])
+                
             ->add('course', EntityType::class, [
                 'class' => Course::class,
                 'choice_label' => function ($course) {
                     return $course->getCourseName();
                 },
-            ])
-            ->add('Submit', SubmitType::class)
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
