972dd5ba8f096cd5f892e76b401096d74fdd8db7
Guillaume <poucetguillaume@gmail.com>
2020-08-25 15:49:49 +0200

    admin dir andmake admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Entity\Course;
 use App\Entity\Session;
 use App\Form\AddUserType;
+use App\Form\EditUserType;
 use App\Form\AddCourseType;
 use App\Form\AddSessionType;
 use App\Repository\UserRepository;
@@ -32,41 +33,7 @@ class AdminController extends AbstractController
         // retrieve sessions info
         $sessions = $sessionRepo->findAll();
 
-        // creating form to add users
-        $newUser = new User;
 
-        $userForm = $this->createForm(AddUserType::class, $newUser);
-
-        $userForm->handleRequest($request);
-        if ($userForm->isSubmitted() && $userForm->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $newUser = $userForm->getData();
-
-            // encode the plain password
-            $newUser->setPassword(
-                $passwordEncoder->encodePassword(
-                    $newUser,
-                    $userForm->get('password')->getData()
-                )
-            );
-
-            $sessions = $userForm->get('sessions')->getData();
-            foreach ($sessions as $session) {
-                $session->addUser($newUser);
-            };
-
-            // dump($session);
-            // exit;
-
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($newUser);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('admin');
-        }
 
         // creating form to add courses
         $newCourse = new Course;
@@ -105,14 +72,13 @@ class AdminController extends AbstractController
             'users' => $users,
             'courses' => $courses,
             'sessions' => $sessions,
-            'userForm' => $userForm->createView(),
             'courseForm' => $courseForm->createView(),
             'sessionForm' => $sessionForm->createView(),
         ]);
     }
 
     /**
-     * @Route("/admin/delete/user/{id}", name="delete_user")
+     * @Route("/admin/delete/user/{id}", name="delete.user")
      */
 
     public function deleteUser(UserRepository $userRepo, $id)
@@ -125,7 +91,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/delete/course/{id}", name="delete_course")
+     * @Route("/admin/delete/course/{id}", name="delete.course")
      */
 
     public function deleteCourse(CourseRepository $courseRepo, $id)
@@ -138,7 +104,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/delete/session/{id}", name="delete_session")
+     * @Route("/admin/delete/session/{id}", name="delete.session")
      */
 
     public function deleteSession(SessionRepository $sessionRepo, $id)
@@ -148,5 +114,121 @@ class AdminController extends AbstractController
         $entityManager->remove($session);
         $entityManager->flush();
         return $this->redirectToRoute('admin');
+    }
+
+    /**
+     * @Route("/admin/users", name="users.management")
+     */
+
+    public function userList(UserRepository $userRepo, SessionRepository $sessionRepo, CourseRepository $courseRepo, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // retrieve users info
+        $users = $userRepo->findAll();
+
+        return $this->render('admin/users_management/index.html.twig', [
+            'controller_name' => 'AdminController',
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/new", name="admin.new.user")
+     */
+
+    public function newUser(UserRepository $userRepo, SessionRepository $sessionRepo, CourseRepository $courseRepo, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // creating form to add users
+        $newUser = new User;
+
+        $userForm = $this->createForm(AddUserType::class, $newUser);
+
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $newUser = $userForm->getData();
+
+            // encode the plain password
+            $newUser->setPassword(
+                $passwordEncoder->encodePassword(
+                    $newUser,
+                    $userForm->get('password')->getData()
+                )
+            );
+
+            $sessions = $userForm->get('sessions')->getData();
+            foreach ($sessions as $session) {
+                $session->addUser($newUser);
+            };
+
+            // dump($session);
+            // exit;
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newUser);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('users.management');
+        }
+
+        return $this->render('admin/users_management/newUser.html.twig', [
+            'controller_name' => 'AdminController',
+            'userForm' => $userForm->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/edit/{id}", name="admin.edit.user")
+     */
+
+    public function editUser(User $user, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $userForm = $this->createForm(EditUserType::class, $user);
+
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $user = $userForm->getData();
+
+            // encode the plain password
+            // $user->setPassword(
+            //     $passwordEncoder->encodePassword(
+            //         $user,
+            //         $userForm->get('password')->getData()
+            //     )
+            // );
+
+            // dump($session);
+            // exit;
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('users.management');
+        }
+
+        return $this->render('admin/users_management/editUser.html.twig', [
+            'controller_name' => 'AdminController',
+            'user' => $user,
+            'userForm' => $userForm->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/make_admin/{id}", name="admin.make.admin")
+     */
+
+    public function makeAdmin(User $user, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user->setRoles(array("ROLE_ADMIN"));
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+        return $this->redirectToRoute('users.management');
     }
 }
--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -3,36 +3,36 @@
 namespace App\Form;
 
 use App\Entity\User;
-use App\Entity\Course;
-use App\Entity\Session;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
-class AddUserType extends AbstractType
+
+class EditUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('email')
-            // ->add('roles')
-            ->add('password', PasswordType::class)
+            // ->add('roles', ChoiceType::class, [
+            //     'choice_label' => 'Admin',
+            //     // 'choices' =>User::ROLE,
+            
+            // ])
+            // ->add('password', PasswordType::class)
             ->add('last_name')
             ->add('first_name')
-            // ->add('birth_date')
+            ->add('birth_date')
             // ->add('isVerified')
-            ->add('sessions', EntityType::class, [
-                'class' => Session::class,
-                'choice_label' => function ($course) {
-                    return $course->getCourse()->getCourseName();
-                },
-                'multiple' => true,
-                'expanded' => true,
-            ])
+            // ->add('sessions', EntityType::class, [
+            //     'class' => Session::class,
+            //     'choice_label' => function ($course) {
+            //         return $course->getCourse()->getCourseName();
+            //     },
+            //     'multiple' => true,
+            //     'expanded' => true,
+            // ])
  
         ;
     }
