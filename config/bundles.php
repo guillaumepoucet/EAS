43d6dad1be846bc9ca12b168a1de97a08254542b
Guillaume <poucetguillaume@gmail.com>
2020-08-28 19:01:20 +0200

    composer datatables

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Omines\DataTablesBundle\DataTablesBundle::class => ['all' => true],
 ];
